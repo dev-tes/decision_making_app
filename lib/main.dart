b5645dcecf85f23dae2ba841dff90c6e65ae6a5b
dev-tes <tesleem.amuda@gmail.com>
2021-11-17 00:28:08 +0100

    customize container

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,56 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue[900],
+            title: Text(
+              'Ask Me Anything'
+            ),
+          ),
+          body: DecisionPage(
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-  
     );
+
+class DecisionPage extends StatefulWidget {
+  @override
+  _DecisionPageState createState() => _DecisionPageState();
+}
+
+class _DecisionPageState extends State<DecisionPage> {
+
+  int imageNumber = 4;
+  void switchImage() {
+    setState(() {
+      imageNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+          color: Colors.blue,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    switchImage();
+                  },
+                  child: Image.asset('images/ball$imageNumber.png'),
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
